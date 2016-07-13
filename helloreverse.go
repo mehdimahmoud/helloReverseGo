343c3b641418b1249983af63af8e6fc130f2ad92
Widisplay <contact@widisplay.com>
2016-07-13 14:23:20 +0100

    initial commit

---
+++ helloreverse.go
@@ -0,0 +1,16 @@
+/*
+ * First Library
+ * Prints the reverse string
+ */
+package main
+
+import (
+	"fmt"
+
+	"github.com/mehdimahmoud/stringutilGo"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("! oG, olleH"))
+	fmt.Println(stringutil.Reverse("Hello, Go!"))
+}
